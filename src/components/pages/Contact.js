2625a0f8a4c9e3ff7d63b44614b6883dc905fb66
Py-Ashraful <ashrafulislam180@gmail.com>
2022-02-09 22:31:00 +0600

    contact form style

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,30 +1,43 @@
 import React from "react";
 import { FaUser } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
+import classes from "../../styles/Contact.module.css";
 
 export default function contact() {
   return (
-    <div className="contactForm">
-      <div className="contactForm__title">
+    <div className={classes.contactForm}>
+      <div className={classes.contactFormTitle}>
         <h1>Contact Me</h1>
       </div>
 
-      <div className="contactForm__body">
+      <div className={classes.contactFormBody}>
         <form>
-          <label htmlFor="userName">
-            <FaUser />
-          </label>
-          <input type="text" id="userName" placeholder="User Name" />
-          <label htmlFor="email">
-            <FaEnvelope />
-          </label>
-          <input type="email" id="email" placeholder="Email" />
-          <textarea
-            name="message"
-            id="message"
-            rows="10"
-            placeholder="Message"
-          ></textarea>
+          <div className={classes.inputContainer}>
+            <label htmlFor="userName">
+              <FaUser />
+            </label>
+            <input type="text" id="userName" placeholder="User Name" />
+          </div>
+          <div className={classes.inputContainer}>
+            <label htmlFor="email">
+              <FaEnvelope />
+            </label>
+            <input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className={classes.inputContainer}>
+            <textarea
+              name="message"
+              id="message"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <input
+            id="submitBtn"
+            class="submitBtn"
+            type="submit"
+            value="Send"
+          ></input>
         </form>
       </div>
     </div>
